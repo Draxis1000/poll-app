e393a2f3fe0daa296005f7d67a10902aca6e86b8
Draxis1000 <floresfranciskyle@yahoo.com>
2022-03-03 03:00:06 +0800

    step5
    added SELECT_RANDOM_VOTE and modified get_random_poll_vote()

--- database.py
+++ database.py
@@ -12,6 +12,7 @@ SELECT_ALL_POLLS = "SELECT * FROM polls;"
 SELECT_POLL_WITH_OPTIONS = """SELECT * FROM polls
 JOIN options ON polls.id = options.poll_id
 WHERE polls.id = %s;"""
+SELECT_RANDOM_VOTE = "SELECT * FROM votes WHERE option_id = %s ORDER BY RANDOM() LIMIT 1;"
 
 INSERT_OPTION = "INSERT INTO options (option_text, poll_id) VALUES %s;"
 INSERT_VOTE = "INSERT INTO votes (username, option_id) VALUES (%s, %s);"
@@ -54,7 +55,8 @@ def get_poll_and_vote_results(connection, poll_id):
 def get_random_poll_vote(connection, option_id):
     with connection:
         with connection.cursor() as cursor:
-            pass
+            cursor.execute(SELECT_RANDOM_VOTE, (option_id,))
+            return cursor.fetchone()
 
 
 def create_poll(connection, title, owner, options):
